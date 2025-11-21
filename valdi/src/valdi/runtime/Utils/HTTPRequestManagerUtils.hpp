056a6d3ce032d17aff58727a3bd366a1143b21c4
Achuth V P <65720815+retrymp3@users.noreply.github.com>
2025-11-21 23:38:59 +0530

    Update HTTPRequestManagerUtils.hpp

--- valdi/src/valdi/runtime/Utils/HTTPRequestManagerUtils.hpp
+++ valdi/src/valdi/runtime/Utils/HTTPRequestManagerUtils.hpp
@@ -11,6 +11,7 @@
 #include "valdi_core/HTTPResponse.hpp"
 #include "valdi_core/cpp/Utils/Function.hpp"
 #include "valdi_core/cpp/Utils/Result.hpp"
+#include "valdi_core/cpp/Utils/StringBox.hpp"
 
 namespace Valdi {
 
@@ -18,6 +19,12 @@ class HTTPRequestManagerUtils {
 public:
     static std::shared_ptr<snap::valdi_core::HTTPRequestManagerCompletion> makeRequestCompletion(
         Function<void(Result<snap::valdi_core::HTTPResponse>)> function);
+    
+    /**
+     * Validates that a URL is safe to request, blocking SSRF attack vectors.
+     * Returns false if the URL should be blocked (localhost, private IPs, cloud metadata, etc.)
+     */
+    static bool isUrlAllowed(const StringBox& url);
 };
 
 } // namespace Valdi
