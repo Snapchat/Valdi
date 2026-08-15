76705c4af78a50e08a014e85d892dc62170c4159
tx3stn <14163530+tx3stn@users.noreply.github.com>
2026-08-16 00:25:13 +0100

    fix: add request manager to standalone

--- valdi/src/valdi/cli_runner/CLIRunner.hpp
+++ valdi/src/valdi/cli_runner/CLIRunner.hpp
@@ -1,5 +1,16 @@
+#pragma once
+
+#include <memory>
+
+namespace snap::valdi_core {
+class HTTPRequestManager;
+}
+
 namespace Valdi {
 
-int valdiCLIRun(const char* scriptPath, int argc, const char** argv);
+int valdiCLIRun(const char* scriptPath,
+                int argc,
+                const char** argv,
+                const std::shared_ptr<snap::valdi_core::HTTPRequestManager>& requestManager = nullptr);
 
 }
---
+++ valdi/test/integration/StandaloneRequestManager_tests.cpp
@@ -0,0 +1,76 @@
+#include "RequestManagerMock.hpp"
+#include "valdi/runtime/Runtime.hpp"
+#include "valdi/standalone_runtime/ValdiStandaloneMain.hpp"
+#include "valdi/standalone_runtime/ValdiStandaloneRuntime.hpp"
+#include "valdi_core/cpp/Utils/ByteBuffer.hpp"
+#include "valdi_core/cpp/Utils/ConsoleLogger.hpp"
+
+#include "JSBridgeTestFixture.hpp"
+#include "gtest/gtest.h"
+
+using namespace Valdi;
+
+namespace ValdiTest {
+
+// A CLI app reaches valdi_http through createValdiStandaloneRuntime, which had no way to install a
+// request manager, so performRequest always failed with "No RequestManager set".
+class StandaloneRequestManagerFixture : public JSBridgeTestFixture {
+protected:
+    StandaloneArguments makeArguments() {
+        StandaloneArguments arguments;
+        arguments.jsBridge = getJsBridge();
+        arguments.logLevel = LogTypeError;
+        return arguments;
+    }
+};
+
+TEST_P(StandaloneRequestManagerFixture, isNullWhenNotSupplied) {
+    auto standaloneRuntime = createValdiStandaloneRuntime(makeArguments());
+
+    ASSERT_EQ(standaloneRuntime->getRuntime().getRequestManager(), nullptr);
+}
+
+TEST_P(StandaloneRequestManagerFixture, reachesTheRuntimeWhenSupplied) {
+    auto requestManager = Valdi::makeShared<RequestManagerMock>(ConsoleLogger::getLogger());
+
+    auto arguments = makeArguments();
+    arguments.requestManager = requestManager;
+
+    auto standaloneRuntime = createValdiStandaloneRuntime(arguments);
+
+    ASSERT_EQ(standaloneRuntime->getRuntime().getRequestManager(), requestManager);
+}
+
+// The end-to-end shape a CLI app exercises: performRequest resolves instead of throwing.
+TEST_P(StandaloneRequestManagerFixture, performRequestSucceedsFromJavaScript) {
+    auto requestManager = Valdi::makeShared<RequestManagerMock>(ConsoleLogger::getLogger());
+    requestManager->addMockedResponse(STRING_LITERAL("http://localhost/"), STRING_LITERAL("GET"), BytesView());
+
+    auto arguments = makeArguments();
+    arguments.requestManager = requestManager;
+
+    auto standaloneRuntime = createValdiStandaloneRuntime(arguments);
+
+    std::string js = "var m = global.require('valdi_http/src/NativeHTTPClient');"
+                     "try {"
+                     "  m.performRequest({ url: 'http://localhost/', method: 'GET', headers: {} }, function () {});"
+                     "  return 'ok';"
+                     "} catch (e) {"
+                     "  return String(e);"
+                     "}";
+
+    auto result = standaloneRuntime->getRuntime().getJavaScriptRuntime()->evaluateScript(
+        makeShared<ByteBuffer>(js)->toBytesView(), STRING_LITERAL("standalone_request_manager_test.js"));
+
+    ASSERT_TRUE(result) << result.description();
+    ASSERT_EQ("ok", result.value().toString());
+}
+
+INSTANTIATE_TEST_SUITE_P(StandaloneRequestManagerTests,
+                         StandaloneRequestManagerFixture,
+                         ::testing::Values(JavaScriptEngineTestCase::Hermes,
+                                           JavaScriptEngineTestCase::QuickJS,
+                                           JavaScriptEngineTestCase::JSCore),
+                         PrintJavaScriptEngineType());
+
+} // namespace ValdiTest
